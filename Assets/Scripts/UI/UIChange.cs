a79319191c2e88f9240e73b650bbdb7e85618ff7
mhggpo <mhggpo@hotmail.com>
2022-06-27 17:15:39 +0800

    FEA: add begin UI scene

---
+++ Assets/Scripts/UI/UIChange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+    public void ExitGame()
+    {
+        BGMController.instance.SaveVolume();
+#if UNITY_EDITOR 
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
